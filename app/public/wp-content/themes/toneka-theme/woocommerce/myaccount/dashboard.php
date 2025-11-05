ca9a84155dda7f7f83b9359e4802041e117f711c
m <jeden-@users.noreply.github.com>
2025-11-05 23:07:55 +0100

    Poprawka dashboard.php - użycie wp_get_current_user()

--- app/public/wp-content/themes/toneka-theme/woocommerce/myaccount/dashboard.php
+++ app/public/wp-content/themes/toneka-theme/woocommerce/myaccount/dashboard.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * My Account dashboard
+ * My Account Dashboard
+ *
+ * Shows the first intro screen on the account dashboard.
  *
  * This template can be overridden by copying it to yourtheme/woocommerce/myaccount/dashboard.php.
  *
@@ -9,7 +11,9 @@
  * @version 4.4.0
  */
 
-defined( 'ABSPATH' ) || exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
 
 $allowed_html = array(
 	'a' => array(
@@ -24,8 +28,25 @@ $allowed_html = array(
 		printf(
 			/* translators: 1: user display name 2: logout url */
 			wp_kses( __( 'Witaj %1$s (nie jesteś %1$s? <a href="%2$s">Wyloguj się</a>)', 'woocommerce' ), $allowed_html ),
-			'<strong>' . esc_html( $current_user->display_name ) . '</strong>',
+			'<strong>' . esc_html( wp_get_current_user()->display_name ) . '</strong>',
 			esc_url( wc_logout_url() )
+		);
+		?>
+	</p>
+
+	<p>
+		<?php
+		/* translators: 1: Orders URL 2: Address URL 3: Account URL. */
+		$dashboard_desc = __( 'Z pulpitu nawigacyjnego konta możesz przeglądać swoje <a href="%1$s">ostatnie zamówienia</a>, zarządzać <a href="%2$s">adresem rozliczeniowym</a> oraz <a href="%3$s">edytować swoje hasło i szczegóły konta</a>.', 'woocommerce' );
+		if ( wc_shipping_enabled() ) {
+			/* translators: 1: Orders URL 2: Addresses URL 3: Account URL. */
+			$dashboard_desc = __( 'Z pulpitu nawigacyjnego konta możesz przeglądać swoje <a href="%1$s">ostatnie zamówienia</a>, zarządzać <a href="%2$s">adresami wysyłkowymi i rozliczeniowymi</a> oraz <a href="%3$s">edytować swoje hasło i szczegóły konta</a>.', 'woocommerce' );
+		}
+		printf(
+			wp_kses( $dashboard_desc, $allowed_html ),
+			esc_url( wc_get_endpoint_url( 'orders' ) ),
+			esc_url( wc_get_endpoint_url( 'edit-address' ) ),
+			esc_url( wc_get_endpoint_url( 'edit-account' ) )
 		);
 		?>
 	</p>
@@ -53,4 +74,3 @@ $allowed_html = array(
 		do_action( 'woocommerce_after_my_account' );
 	?>
 </div>
-
