27a926469d89f5bca1321a2008dddb1b838d9f96
m <jeden-@users.noreply.github.com>
2025-11-05 13:40:54 +0100

    Ujednolicenie stylów na całym procesie zamówienia
    - Stworzenie szablonu logowania/rejestracji (myaccount/form-login.php)
    - Dodanie stylów dla strony 'Moje konto' i wszystkich podstron
    - Ujednolicenie stylów nagłówków we wszystkich formularzach WooCommerce
    - Rozszerzenie stylów dla strony Thank You
    - Globalne style dla wszystkich formularzy, przycisków i powiadomień WooCommerce
    - Style dla podstron konta: zamówienia, adresy, edycja konta, metody płatności
    - Spójne użycie zmiennych CSS i fontów z tematu
    - Responsywność dla wszystkich nowych stron

---
+++ app/public/wp-content/themes/toneka-theme/woocommerce/myaccount/form-login.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * Login Form
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/myaccount/form-login.php.
+ *
+ * @see     https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 9.9.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header( 'shop' );
+
+do_action( 'woocommerce_before_customer_login_form' ); ?>
+
+<div class="toneka-myaccount-page">
+	<div class="toneka-myaccount-container">
+		
+		<?php if ( 'yes' === get_option( 'woocommerce_enable_myaccount_registration' ) ) : ?>
+
+		<div class="toneka-login-register-grid">
+			
+			<div class="toneka-login-section">
+				<h2 class="toneka-section-title"><?php esc_html_e( 'Logowanie', 'woocommerce' ); ?></h2>
+
+				<form class="toneka-myaccount-form woocommerce-form woocommerce-form-login login" method="post" novalidate>
+
+					<?php do_action( 'woocommerce_login_form_start' ); ?>
+
+					<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+						<label for="username"><?php esc_html_e( 'Nazwa użytkownika lub adres e-mail', 'woocommerce' ); ?>&nbsp;<span class="required" aria-hidden="true">*</span></label>
+						<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="username" id="username" autocomplete="username" value="<?php echo ( ! empty( $_POST['username'] ) && is_string( $_POST['username'] ) ) ? esc_attr( wp_unslash( $_POST['username'] ) ) : ''; ?>" required aria-required="true" />
+					</p>
+					<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+						<label for="password"><?php esc_html_e( 'Hasło', 'woocommerce' ); ?>&nbsp;<span class="required" aria-hidden="true">*</span></label>
+						<input class="woocommerce-Input woocommerce-Input--text input-text" type="password" name="password" id="password" autocomplete="current-password" required aria-required="true" />
+					</p>
+
+					<?php do_action( 'woocommerce_login_form' ); ?>
+
+					<p class="form-row form-row-checkbox">
+						<label class="woocommerce-form__label woocommerce-form__label-for-checkbox woocommerce-form-login__rememberme">
+							<input class="woocommerce-form__input woocommerce-form__input-checkbox" name="rememberme" type="checkbox" id="rememberme" value="forever" /> <span><?php esc_html_e( 'Zapamiętaj mnie', 'woocommerce' ); ?></span>
+						</label>
+						<?php wp_nonce_field( 'woocommerce-login', 'woocommerce-login-nonce' ); ?>
+					</p>
+
+					<p class="form-row">
+						<button type="submit" class="toneka-button toneka-button-primary woocommerce-button button woocommerce-form-login__submit" name="login" value="<?php esc_attr_e( 'Log in', 'woocommerce' ); ?>"><?php esc_html_e( 'Zaloguj się', 'woocommerce' ); ?></button>
+					</p>
+
+					<p class="woocommerce-LostPassword lost_password">
+						<a href="<?php echo esc_url( wp_lostpassword_url() ); ?>"><?php esc_html_e( 'Nie pamiętasz hasła?', 'woocommerce' ); ?></a>
+					</p>
+
+					<?php do_action( 'woocommerce_login_form_end' ); ?>
+
+				</form>
+			</div>
+
+			<div class="toneka-register-section">
+				<h2 class="toneka-section-title"><?php esc_html_e( 'Rejestracja', 'woocommerce' ); ?></h2>
+
+				<form method="post" class="toneka-myaccount-form woocommerce-form woocommerce-form-register register" <?php do_action( 'woocommerce_register_form_tag' ); ?> >
+
+					<?php do_action( 'woocommerce_register_form_start' ); ?>
+
+					<?php if ( 'no' === get_option( 'woocommerce_registration_generate_username' ) ) : ?>
+
+						<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+							<label for="reg_username"><?php esc_html_e( 'Nazwa użytkownika', 'woocommerce' ); ?>&nbsp;<span class="required" aria-hidden="true">*</span></label>
+							<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="username" id="reg_username" autocomplete="username" value="<?php echo ( ! empty( $_POST['username'] ) ) ? esc_attr( wp_unslash( $_POST['username'] ) ) : ''; ?>" required aria-required="true" />
+						</p>
+
+					<?php endif; ?>
+
+					<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+						<label for="reg_email"><?php esc_html_e( 'Adres e-mail', 'woocommerce' ); ?>&nbsp;<span class="required" aria-hidden="true">*</span></label>
+						<input type="email" class="woocommerce-Input woocommerce-Input--text input-text" name="email" id="reg_email" autocomplete="email" value="<?php echo ( ! empty( $_POST['email'] ) ) ? esc_attr( wp_unslash( $_POST['email'] ) ) : ''; ?>" required aria-required="true" />
+					</p>
+
+					<?php if ( 'no' === get_option( 'woocommerce_registration_generate_password' ) ) : ?>
+
+						<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+							<label for="reg_password"><?php esc_html_e( 'Hasło', 'woocommerce' ); ?>&nbsp;<span class="required" aria-hidden="true">*</span></label>
+							<input type="password" class="woocommerce-Input woocommerce-Input--text input-text" name="password" id="reg_password" autocomplete="new-password" required aria-required="true" />
+						</p>
+
+					<?php else : ?>
+
+						<p><?php esc_html_e( 'Link do ustawienia nowego hasła zostanie wysłany na Twój adres e-mail.', 'woocommerce' ); ?></p>
+
+					<?php endif; ?>
+
+					<?php do_action( 'woocommerce_register_form' ); ?>
+
+					<p class="woocommerce-form-row form-row">
+						<?php wp_nonce_field( 'woocommerce-register', 'woocommerce-register-nonce' ); ?>
+						<button type="submit" class="toneka-button toneka-button-primary woocommerce-Button woocommerce-button button woocommerce-form-register__submit" name="register" value="<?php esc_attr_e( 'Register', 'woocommerce' ); ?>"><?php esc_html_e( 'Zarejestruj się', 'woocommerce' ); ?></button>
+					</p>
+
+					<?php do_action( 'woocommerce_register_form_end' ); ?>
+
+				</form>
+			</div>
+
+		</div>
+
+		<?php else : ?>
+
+		<div class="toneka-login-single">
+			<h2 class="toneka-section-title"><?php esc_html_e( 'Logowanie', 'woocommerce' ); ?></h2>
+
+			<form class="toneka-myaccount-form woocommerce-form woocommerce-form-login login" method="post" novalidate>
+
+				<?php do_action( 'woocommerce_login_form_start' ); ?>
+
+				<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+					<label for="username"><?php esc_html_e( 'Nazwa użytkownika lub adres e-mail', 'woocommerce' ); ?>&nbsp;<span class="required" aria-hidden="true">*</span></label>
+					<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="username" id="username" autocomplete="username" value="<?php echo ( ! empty( $_POST['username'] ) && is_string( $_POST['username'] ) ) ? esc_attr( wp_unslash( $_POST['username'] ) ) : ''; ?>" required aria-required="true" />
+				</p>
+				<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+					<label for="password"><?php esc_html_e( 'Hasło', 'woocommerce' ); ?>&nbsp;<span class="required" aria-hidden="true">*</span></label>
+					<input class="woocommerce-Input woocommerce-Input--text input-text" type="password" name="password" id="password" autocomplete="current-password" required aria-required="true" />
+				</p>
+
+				<?php do_action( 'woocommerce_login_form' ); ?>
+
+				<p class="form-row form-row-checkbox">
+					<label class="woocommerce-form__label woocommerce-form__label-for-checkbox woocommerce-form-login__rememberme">
+						<input class="woocommerce-form__input woocommerce-form__input-checkbox" name="rememberme" type="checkbox" id="rememberme" value="forever" /> <span><?php esc_html_e( 'Zapamiętaj mnie', 'woocommerce' ); ?></span>
+					</label>
+					<?php wp_nonce_field( 'woocommerce-login', 'woocommerce-login-nonce' ); ?>
+				</p>
+
+				<p class="form-row">
+					<button type="submit" class="toneka-button toneka-button-primary woocommerce-button button woocommerce-form-login__submit" name="login" value="<?php esc_attr_e( 'Log in', 'woocommerce' ); ?>"><?php esc_html_e( 'Zaloguj się', 'woocommerce' ); ?></button>
+				</p>
+
+				<p class="woocommerce-LostPassword lost_password">
+					<a href="<?php echo esc_url( wp_lostpassword_url() ); ?>"><?php esc_html_e( 'Nie pamiętasz hasła?', 'woocommerce' ); ?></a>
+				</p>
+
+				<?php do_action( 'woocommerce_login_form_end' ); ?>
+
+			</form>
+		</div>
+
+		<?php endif; ?>
+
+	</div>
+</div>
+
+<?php do_action( 'woocommerce_after_customer_login_form' ); ?>
+
+<?php get_footer( 'shop' ); ?>
+
